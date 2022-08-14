58d9df6221d2c1ca67d8a176c67c8c5b3b28dbdc
Paul Laxon <pdl1963@gmail.com>
2022-08-14 03:09:28 -0400

    add optional

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,38 +4,68 @@ module Api
   module V1
     # users controller
     class Api::V1::UsersController < ApplicationController
-      class CannotDeleteError < StandardError; end
+      class MultipleUsersError < StandardError; end
+      before_action :all_users
 
       def index
-        find_users(request)
+        find_users_by_query_params(request)
+        raise(MultipleUsersError, 'Error: Too many users found.') if @users.size > 1 && !@return_multiple
+
         render json: @users
+      rescue MultipleUsersError => e
+        render plain: e.message
       end
 
       def create
+        find_users_by_query_params(request)
+        raise(MultipleUsersError, 'Error: User already exists.') if @user
+
         @user = User.new(user_params)
-        @user&.save
+        @users << @user
+
         render json: @user
+      rescue MultipleUsersError => e
+        render plain: e.message
       end
 
       def destroy
-        find_users(request)
+        find_users_by_query_params(request)
+        raise(MultipleUsersError, 'Error: Cannot delete, more than 1 user was found.') unless @users.size == 1
 
-        raise(CannotDeleteError, 'Cannot delete, more than 1 user returned.') unless @users.size == 1
-
-        @user&.destroy
-      rescue CannotDeleteError => e
+        @users&.delete(@user)
+      rescue MultipleUsersError => e
         render plain: e.message
       end
 
       private
 
-      def find_users(request)
+      def all_users
+        @all_users = User.all
+      end
+
+      def find_users_by_query_params(request)
         conditions = {}
         request&.query_parameters&.each do |user_field, value|
           conditions[user_field.to_sym] = value.to_s
         end
-        @users = User.where(conditions)
+        find_users(conditions)
+      end
+
+      def find_users(conditions)
+        @return_multiple = to_bool(conditions.delete(:return_multiple))
+        @users = @all_users&.select do |user|
+          conditions.each do |k, v|
+            break if user[k] != v
+          end
+        end
+
         @user = @users[0] if @users.size == 1
+      end
+
+      def to_bool(str)
+        return true if str == 'true'
+
+        false
       end
 
       def user_params
