7570562e0b5fc02d380f2dfa74863f34418d6aba
Paul Laxon <pdl1963@gmail.com>
2022-08-24 06:22:02 -0400

    clean up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,65 +4,50 @@ module Api
   module V1
     # users controller
     class Api::V1::UsersController < ApplicationController
-      class MultipleUsersError < StandardError; end
+      class FindUsersError < StandardError; end
       before_action :fetch_all_users
 
       def index
         find_users_by_query_params(request)
 
-        raise(MultipleUsersError, 'Error: Too many users found.') if @users.size > 1 && !@return_multiple
-        raise(MultipleUsersError, 'Error: No user was found.') if @users.size.zero?
+        raise(FindUsersError, 'Error: Too many users found.') if @users.size > 1 && !@return_multiple
+        raise(FindUsersError, 'Error: No user was found.') if @users.size.zero?
 
         render json: @users
-      rescue MultipleUsersError => e
+      rescue FindUsersError => e
         render plain: e.message
       end
 
       def create
         @user = User.new(user_params)
-        raise(MultipleUsersError, 'Error: User already exists.') if @all_users.include?(@user)
+        raise(FindUsersError, 'Error: User already exists.') if @all_users.include?(@user)
 
         @all_users << @user
         Rails.cache.write(:all_users, @all_users)
 
         render json: @all_users
-      rescue MultipleUsersError => e
+      rescue FindUsersError => e
         render plain: e.message
       end
 
       def destroy
         find_users_by_query_params(request)
-        raise(MultipleUsersError, 'Error: Cannot delete, more than 1 user was found.') if @users.size > 1
-        raise(MultipleUsersError, 'Error: Cannot delete, no user was found.') if @users.size.zero?
+        raise(FindUsersError, 'Error: Cannot delete, more than 1 user was found.') if @users.size > 1
+        raise(FindUsersError, 'Error: Cannot delete, no user was found.') if @users.size.zero?
 
         @all_users.delete(@user)
         Rails.cache.write(:all_users, @all_users)
 
         render json: @all_users
-      rescue MultipleUsersError => e
+      rescue FindUsersError => e
         render plain: e.message
       end
 
       private
 
       def fetch_all_users
-        users_cache_key = Rails.cache.instance_variable_get(:@data).keys
-        puts("users cache: #{users_cache_key}")
         @all_users = Rails.cache.fetch(:all_users)
-        puts("all users: #{@all_users}")
-        return if @all_users.present?
-
-        john1 = User.new(last_name: 'Doe', first_name: 'John', email: 'jdoe1@example.com', gov_id_number: '11111111', gov_id_type: 'licence')
-        john2 = User.new(last_name: 'Doe', first_name: 'John', email: 'jdoe2@example.com', gov_id_number: '22222222', gov_id_type: 'licence')
-        mary = User.new(last_name: 'Doe', first_name: 'Mary', email: 'mdoe@example.com', gov_id_number: '33333333', gov_id_type: 'licence')
-        jill1 = User.new(last_name: 'Smith', first_name: 'Jill', email: 'jsmith@example.com', gov_id_number: '44444444', gov_id_type: 'licence')
-        jill2 = User.new(last_name: 'Johnson', first_name: 'Jill', email: 'jjohnson@example.com', gov_id_number: '55555555', gov_id_type: 'licence')
-        @all_users = [john1, john2, mary, jill1, jill2]
-        Rails.cache.write(:all_users, @all_users)
-
-        puts 'new users'
-        @all_users.each { |u| puts u.to_s }
-        puts
+        @all_users = [] if @all_users.blank?
       end
 
       def find_users_by_query_params(request)
